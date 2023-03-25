97943d34e83cfb9f26d23319bb315d42c94c0959
MennaAnwar <menna_m20@yahoo.com>
2023-03-25 08:16:54 -0700

    putchar

--- _putchar.c
+++ _putchar.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _putchar uses a local buffer of 1024 to call write
- * as little as possible
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
@@ -10,18 +9,6 @@
  */
 int _putchar(char c)
 {
-	static char buf[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buf, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
+	return (write(1, &c, 1));
+}
 }
