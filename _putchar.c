b94fb8a10a0f316c38a4d021c5ff119007b51162
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-03-25 18:02:35 +0200

    .

--- _putchar.c
+++ _putchar.c
@@ -5,32 +5,9 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error -1.
  */
 int _putchar(char c)
 {
-	return (buffer(c));
-}
-
-/**
- * buffer - Save the character in a buffer
- * @c: Character
- *
- * Return: 1
- **/
-int buffer(char c)
-{
-	static char buf[1024];
-	static int i;
-
-	if (c == -1 || i == 1024)
-	{
-		write(1, buf, i);
-		i = 0;
-	}
-
-	if (c != -1)
-		buf[i++] = c;
-
-	return (1);
+	return (write(1, &c, 1));
 }
