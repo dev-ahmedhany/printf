ef1f9f8dfe8102ea8b62670b71dd439c9361591a
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-03-25 17:53:40 +0200

    task 4

--- _putchar.c
+++ _putchar.c
@@ -5,9 +5,32 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * On error -1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return (buffer(c));
+}
+
+/**
+ * buffer - Save the character in a buffer
+ * @c: Character
+ *
+ * Return: 1
+ **/
+int buffer(char c)
+{
+	static char buffer[1024];
+	static int i;
+
+	if (c == -1 || i == 1024)
+	{
+		write(1, buffer, i);
+		i = 0;
+	}
+
+	if (c != -1)
+		buffer[i++] = c;
+
+	return (1);
 }
