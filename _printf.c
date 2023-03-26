0da73960604a4d2d753a8327ba1c721c517c1a4e
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-03-26 22:56:52 +0200

    _puts

--- _printf.c
+++ _printf.c
@@ -27,17 +27,3 @@ int _printf(const char *format, ...)
 
 	return (size);
 }
-
-/**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- * Return: number of chars written
- */
-int _puts(char *str)
-{
-	register int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	return (i);
-}
--- _putchar.c
+++ _putchar.c
@@ -25,3 +25,17 @@ int _putchar(char c)
 	}
 	return (1);
 }
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
