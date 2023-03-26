e97c0a6d77140938e10faebbd3338848925bc345
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-03-26 23:10:49 +0200

    convert

--- printf_pointer.c
+++ printf_pointer.c
@@ -20,39 +20,3 @@ int printf_pointer(va_list list)
 	count += _puts(str);
 	return (count);
 }
-
-
-/**
- * dec_to_hex - prints an hexgecimal number.
- * @num: arguments.
- * Return: counter.
- */
-int dec_to_hex(unsigned long int num)
-{
-	long int i;
-	long int *array;
-	long int counter = 0;
-	unsigned long int temp = num;
-
-	while (num / 16 != 0)
-	{
-		num /= 16;
-		counter++;
-	}
-	counter++;
-	array = malloc(counter * sizeof(long int));
-
-	for (i = 0; i < counter; i++)
-	{
-		array[i] = temp % 16;
-		temp /= 16;
-	}
-	for (i = counter - 1; i >= 0; i--)
-	{
-		if (array[i] > 9)
-			array[i] = array[i] + 39;
-		_putchar(array[i] + '0');
-	}
-	free(array);
-	return (counter);
-}
